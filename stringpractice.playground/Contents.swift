588b4c5abfdcf333c835641e5c4950792b4184a0
dfenjves <fenjves.daniel@gmail.com>
2016-07-24 11:53:44 -0400

    solutions

---
+++ stringpractice.playground/Contents.swift
@@ -0,0 +1,29 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var favorite:String = "Hello"
+
+favorite.uppercaseString
+
+"SHHHHH - We're in the Library!!!".lowercaseString
+
+"supercalifragilisticexpialidocious".characters.count
+//or
+"supercalifragilisticexpialidocious".length
+
+
+"margaret heafield hamilton".capitalizedString
+
+"I just won the powerball jackpot.".stringByReplacingOccurrencesOfString(".", withString: "!")
+
+var z1:String = "zip"
+var z2:String = "zap"
+var z3:String = "zop"
+
+z1 + z2 + z3
+
+"\(z1) \(z2) \(z3)"
+
+"Programming is so mulch fun!".stringByReplacingOccurrencesOfString("l", withString: "")
+
